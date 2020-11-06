72db67672a9a7c2e989069780113953dadfcdcbd
Kucing <dilkq@gmail.com>
2020-11-06 10:25:20 +0700

    adding swiper

--- components/Swiper/swiper.js
+++ components/Swiper/swiper.js
@@ -1,25 +1,48 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
 
-// import 'swiper/swiper.scss';
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
 
 const SwiperComp = () => {
   return (
-    <div className="flex">
+      <div className="w-11/12 h-full flex justify-center item-center my-0 mx-0 px-6">
       <Swiper
         spaceBetween={50}
         slidesPerView={3}
-        onSlideChange={() => console.log('slide change')}
+        navigation
+        pagination={{ clickable: true }}
+        scrollbar={{ draggable: true }}
         onSwiper={(swiper) => console.log(swiper)}
+        onSlideChange={() => console.log('slide change')}
       >
-        <SwiperSlide className="w-56 h-56 bg-red-400">Slide 1</SwiperSlide>
-        <SwiperSlide className="w-56 h-56 bg-blue-400">Slide 2</SwiperSlide>
-        <SwiperSlide className="w-56 h-56 bg-red-400">Slide 1</SwiperSlide>
-        <SwiperSlide className="w-56 h-56 bg-blue-400">Slide 2</SwiperSlide>
-       </Swiper>
-    </div>
+        <SwiperSlide className="">
+          <img className="w-64 h-full" src="/promotion/promotion1.jpg" />
+        </SwiperSlide>
+        <SwiperSlide className="">
+          <img className="w-64 h-full" src="/promotion/promotion2.jpg"/>
+        </SwiperSlide>
+        <SwiperSlide className="">
+          <img className="w-64 h-full" src="/promotion/promotion3.jpg"/>
+        </SwiperSlide>
+        <SwiperSlide className="">
+          <img className="w-64 h-full" src="/promotion/promotion4.jpg"/>
+        </SwiperSlide>
+        <SwiperSlide className="">
+          <img className="w-64 h-full" src="/promotion/promotion5.jpg"/>
+        </SwiperSlide>
+        <SwiperSlide className="">
+          <img className="w-64 h-full" src="/promotion/promotion6.jpg"/>
+        </SwiperSlide>
+        <SwiperSlide className="">
+          <img className="w-64 h-full" src="/promotion/promotion7.jpg"/>
+        </SwiperSlide>
+        <SwiperSlide className="">
+          <img className="w-64 h-full" src="/promotion/promotion8.jpg"/>
+        </SwiperSlide>
+      </Swiper>
+      </div>
   )
 }
-
 
 
 export default SwiperComp
