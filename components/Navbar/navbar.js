a2f024270fcde1c6d0c76b65bfb0f1235cdeddd1
Kucing <dilkq@gmail.com>
2020-11-07 14:56:28 +0700

    move promotion page and code to index

--- components/Navbar/navbar.js
+++ components/Navbar/navbar.js
@@ -1,20 +1,21 @@
+
 import Link from 'next/link'
 
 const Navbar = () => {
   return (
-    <div className="flex w-11/12 md:w-full">
-      <nav className="fixed mx-auto shadow top-0 inset-x-auto z-10 w-full py-4 bg-white" >
+    <div className="flex items-center justify-center">
+      <nav className="fixed bg-white shadow w-full top-0 right-0 z-10 p-4 md:px-20">
 
-        <div className="flex justify-between w-11/12 lg:w-3/4 mx-auto my-auto px-4hx">
+        <div className="flex justify-between w-3/4 mx-auto my-auto">
           <div className="flex justify-start ml-0">
+
             <Link href="/">
-            <div className="flex lg:mx-0">
-              <a>
-              <img src="/hisana-logo.svg" className="w-16 h-auto mx-auto cursor-pointer" />
-              </a>
-            </div>
+              <div className="flex lg:mx-0">
+                <a>
+                  <img src="/hisana-logo.svg" className="w-16 h-auto mx-auto cursor-pointer" />
+                </a>
+              </div>
             </Link>
-           
 
             <div className="hidden lg:flex my-auto">
               <Link href="/product">
@@ -27,7 +28,7 @@ const Navbar = () => {
           </div>
 
           <div className="my-auto">
-            <img src="/menu.svg" className="h-6 cursor-pointer" />
+            <img src="/menu.svg" className="w-auto h-6 cursor-pointer" />
           </div>
         </div>
       </nav>
