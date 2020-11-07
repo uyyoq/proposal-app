0c8f10c7f0b14be4a7a2a678b04002f927bd087b
Kucing <dilkq@gmail.com>
2020-11-07 14:21:02 +0700

    fix navbar contact

--- components/Navbar/navbar.js
+++ components/Navbar/navbar.js
@@ -2,31 +2,27 @@ import Link from 'next/link'
 
 const Navbar = () => {
   return (
-    <div className="flex items-center justify-center">
-      <nav className="fixed bg-white shadow w-full top-0 z-10 p-4">
+    <div className="flex w-11/12 md:w-full">
+      <nav className="fixed mx-auto shadow top-0 inset-x-auto z-10 w-full py-4 bg-white" >
 
-        <div className="flex justify-between w-3/4 mx-auto my-auto">
+        <div className="flex justify-between w-11/12 lg:w-3/4 mx-auto my-auto px-4hx">
           <div className="flex justify-start ml-0">
-            <Link href="/">
-              <div className="flex lg:mx-0">
-                <a>
-                  <img src="/hisana-logo.svg" className="w-16 h-auto mx-auto cursor-pointer" />
-                </a>
-              </div>
-            </Link>
+            <div className="flex lg:mx-0">
+              <img src="/hisana-logo.svg" className="w-16 h-auto mx-auto cursor-pointer" />
+            </div>
 
             <div className="hidden lg:flex my-auto">
-              <Link href="/contact">
-                <a className=" mx-8 text-red-600 hover:text-red-700 font-extrabold cursor-pointer">Contact</a>
+              <Link href="/promotion">
+                <a className="ml-8 text-red-600 hover:text-red-700 font-extrabold cursor-pointer">Promotion</a>
               </Link>
-              <Link href="/product">
-                <a className=" text-red-600 hover:text-red-700 font-extrabold cursor-pointer">Product</a>
+              <Link href="/contact">
+                <a className="ml-8 text-red-600 hover:text-red-700 font-extrabold cursor-pointer">Contact</a>
               </Link>
             </div>
           </div>
 
           <div className="my-auto">
-            <img src="/menu.svg" className="w-auto h-6 cursor-pointer" />
+            <img src="/menu.svg" className="h-6 cursor-pointer" />
           </div>
         </div>
       </nav>
@@ -36,4 +32,3 @@ const Navbar = () => {
 }
 
 export default Navbar;
-
