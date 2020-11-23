5cfae8fd1eee1f55048296d2165d744fe8c3cca3
Kucing <dilkq@gmail.com>
2020-11-23 16:45:40 +0700

    change font

--- components/Navbar/navbar.js
+++ components/Navbar/navbar.js
@@ -31,9 +31,9 @@ const Navbar = () => {
             <div className="ml-8 mt-2">
               <div className="dropdown inline-block relative cursor-pointer">
                 <button className="text-red-600 hover:text-red-700 font-extrabold inline-flex">Dropdown</button>
-                <ul className="dropdown-menu absolute hidden text-red-600 pt-1">
-                  <li><a className="rounded-t bg-white hover:text-red-700 py-2 px-4 block whitespace-no-wrap" >Menu 1</a></li>
-                  <li><a className="bg-white hover:text-red-700 py-2 px-4 block whitespace-no-wrap" >Menu 2</a></li>
+                <ul className="dropdown-menu absolute hidden text-red-600  shadow-2xl pt-1 bg-white">
+                  <li><a className="rounded-t hover:text-red-700 py-2 px-4 hover:font-semibold hover: block whitespace-no-wrap" >Menu 1</a></li>
+                  <li><a className=" hover:text-red-700 py-2 px-4 block hover:font-semibold hover: whitespace-no-wrap" >Menu 2</a></li>
                 </ul>
               </div>
             </div>
