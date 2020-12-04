13308a888277c184c412bc5865d74c4b1c20c22c
Kucing <dilkq@gmail.com>
2020-12-04 15:24:11 +0700

    merge navbar dan title responsive

--- components/Swiper/swiper.js
+++ components/Swiper/swiper.js
@@ -8,6 +8,7 @@ const SwiperComp = () => {
   return (
     <div className="w-11/12 h-full flex justify-center item-center my-0 mx-0 px-6">
       <Swiper
+        className="hidden md:block"
         spaceBetween={23}
         slidesPerView={3}
         mousewheel
@@ -18,10 +19,10 @@ const SwiperComp = () => {
           <Card card="/card/card1.jpg" menu="Hisana Dada Ayam" price="Rp.11.000" sale="4%" />
         </SwiperSlide>
         <SwiperSlide className="py-8 px-2">
-          <Card card="/card/card2.jpg" menu="Hisana Sayap Ayam" price="Rp.9.000" sale="10%"/>
+          <Card card="/card/card2.jpg" menu="Hisana Sayap Ayam" price="Rp.9.000" sale="10%" />
         </SwiperSlide>
         <SwiperSlide className="py-8 px-2">
-          <Card card="/card/card3.jpg" menu="Hisana Paha Bawah" price="Rp.10.000" sale="2%"/>
+          <Card card="/card/card3.jpg" menu="Hisana Paha Bawah" price="Rp.10.000" sale="2%" />
         </SwiperSlide>
         <SwiperSlide className="py-8 px-2">
           <Card card="/card/card2.jpg" menu="Hisana Paha Atas" price="Rp.11.000" sale="20%" />
@@ -30,7 +31,34 @@ const SwiperComp = () => {
           <Card card="/card/card5.jpg" menu="Hisana Ayam Mini" price="Rp.6.000" sale="13%" />
         </SwiperSlide>
         <SwiperSlide className="py-8 px-2">
-          <Card card="/card/card1.jpg" menu="Hisana Dada Ayam" price="Rp.11.000" sale="9%"/>
+          <Card card="/card/card1.jpg" menu="Hisana Dada Ayam" price="Rp.11.000" sale="9%" />
+        </SwiperSlide>
+      </Swiper>
+      <Swiper
+        className="block md:hidden"
+        spaceBetween={23}
+        slidesPerView={1}
+        mousewheel
+        scrollbar
+        onSwiper={(swiper) => console.log(swiper)}
+      >
+        <SwiperSlide className="py-8 px-2">
+          <Card card="/card/card1.jpg" menu="Hisana Dada Ayam" price="Rp.11.000" sale="4%" />
+        </SwiperSlide>
+        <SwiperSlide className="py-8 px-2">
+          <Card card="/card/card2.jpg" menu="Hisana Sayap Ayam" price="Rp.9.000" sale="10%" />
+        </SwiperSlide>
+        <SwiperSlide className="py-8 px-2">
+          <Card card="/card/card3.jpg" menu="Hisana Paha Bawah" price="Rp.10.000" sale="2%" />
+        </SwiperSlide>
+        <SwiperSlide className="py-8 px-2">
+          <Card card="/card/card2.jpg" menu="Hisana Paha Atas" price="Rp.11.000" sale="20%" />
+        </SwiperSlide>
+        <SwiperSlide className="py-8 px-2">
+          <Card card="/card/card5.jpg" menu="Hisana Ayam Mini" price="Rp.6.000" sale="13%" />
+        </SwiperSlide>
+        <SwiperSlide className="py-8 px-2">
+          <Card card="/card/card1.jpg" menu="Hisana Dada Ayam" price="Rp.11.000" sale="9%" />
         </SwiperSlide>
       </Swiper>
     </div>
--- pages/chart.js
+++ pages/chart.js
@@ -1,8 +1,5 @@
-import React, { useState, useEffect } from "react";
-import {
-  LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer,
-} from 'recharts';
-import Navbar from "../components/Navbar/navbar";
+import {LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer,} from 'recharts';
+import Navbar from '../components/Navbar/navbar';
 
 const data = [
   {
@@ -27,58 +24,54 @@ const data = [
     name: 'jul', burger: 3490, chiken: 7300, hottang: 4800, pizza: 7300, amt: 2100,
   },
   {
-    name: 'agt', burger: 3490, chiken: 4300, hottang: 5100, pizza: 5600, amt: 2100,
+    name: 'agt', burger: 3290, chiken: 2300, hottang: 5100, pizza: 5600, amt: 2100,
   },
   {
-    name: 'sep', burger: 3490, chiken: 4300, hottang: 5000, pizza: 5000, amt: 2100,
+    name: 'sep', burger: 4490, chiken: 4900, hottang: 5000, pizza: 5000, amt: 2100,
   },
   {
-    name: 'oct', burger: 3490, chiken: 4300, hottang: 7000, pizza: 4300, amt: 2100,
+    name: 'oct', burger: 3990, chiken: 4300, hottang: 7000, pizza: 4300, amt: 2100,
   },
   {
-    name: 'nov', burger: 3490, chiken: 8300, hottang: 3400, pizza: 3700, amt: 2100,
+    name: 'nov', burger: 2190, chiken: 8300, hottang: 3400, pizza: 3700, amt: 2100,
   },
   {
-    name: 'dec', burger: 3490, chiken: 4300, hottang: 4200, pizza: 5600, amt: 2100,
+    name: 'dec', burger: 6490, chiken: 4300, hottang: 4200, pizza: 5600, amt: 2100,
   },
 ];
 
-const Chart = (props) => {
-  const [navbar, setnavbar] = useState(true)
+const Chart = () => {
 
-  const handleNavbar = () => {
-    setnavbar(!navbar);
-  }
-  return (
-    <>
-      <Navbar/>
-      <p className="pt-24 text-center font-bold text-lg text-red-600 ">Data Penjulan</p>
-      <div style={{ width: 600, height: 400 }} className="flex justify-center pt-8 ml-16 md:ml-32 lg:ml-64 right-0">
-        <ResponsiveContainer>
-          <LineChart
-            width={800}
-            height={400}
-            data={data}
-            margin={{
-              top: 5, right: 100, left: 30, bottom: 5,
-            }}
-          >
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="name" />
-            <YAxis />
-            <Tooltip />
-            <Legend />
-            <Line type="monotone" dataKey="burger" stroke="#FFFF00" activeDot={{ r: 8 }} />
-            <Line type="monotone" dataKey="chiken" stroke="#FF0000" />
-            <Line type="monotone" dataKey="pizza" stroe="#FF00FF" />
-            <Line type="monotone" dataKey="hottang" stroke="#00FF00" />
-          </LineChart>
-        </ResponsiveContainer>
-      </div>
+return (
+  <>
+  <Navbar />
+    <p className="hidden md:block pt-24 text-center font-bold text-lg text-red-600 ">Data Penjulan</p>
+    <div style={{ width: 600, height: 400 }} className="flex justify-center mt-24 md:mt-8 ml-16 md:ml-32 lg:ml-64 right-0">
+      <ResponsiveContainer>
+        <LineChart
+          width={800}
+          height={400}
+          data={data}
+          margin={{
+            top: 5, right: 100, left: 30, bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line type="monotone" dataKey="burger" stroke="#FFFF00" activeDot={{ r: 8 }} />
+          <Line type="monotone" dataKey="chiken" stroke="#FF0000" />
+          <Line type="monotone" dataKey="pizza" stroe="#FF00FF" />
+          <Line type="monotone" dataKey="hottang" stroke="#00FF00" />
+        </LineChart>
+      </ResponsiveContainer>
+    </div>
 
-    </>
+  </>
 
-  )
+)
 };
 
 export default Chart;
