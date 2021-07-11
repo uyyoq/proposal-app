e9564d94ddfbe6fe85d01d23fd3e2edcf383030d
indra  susila <indra.susila@logique.co.id>
2021-07-11 21:02:07 +0700

    add itarable map card

--- pages/covid-19.js
+++ pages/covid-19.js
@@ -1,58 +1,53 @@
-import React from "react"
+import React from "react";
 
+const mati = "/icon/mati.svg";
+const dirawat = "/icon/dirawat.svg";
+const positif = "/icon/positif.svg";
+const sembuh = "/icon/sembuh.svg";
 
-const mati = "/icon/mati.svg"
-const dirawat = "/icon/dirawat.svg"
-const positif = "/icon/positif.svg"
-const sembuh = "/icon/sembuh.svg"
+const data = [
+  {
+    judul: "Meninggal",
+    jumlah: "12121222",
+    img: mati,
+  },
+  {
+    judul: "Positif",
+    jumlah: "12122",
+    img: positif,
+  },
+
+  // ..... dan seterusnya
+];
+
+const Card = ({ judul, jumlah, img }) => {
+  return (
+    <div className="flex flex-col w-32 h-32 border-2 border-red-500 py-2 px-2 rounded-md items-center">
+      <p className="font-bold text-sm text-red-500">{judul}</p>
+      <img src={img} alt={judul} className="h-10 w-10 my-2"></img>
+      <div>
+        <p className="font-bold text-sm text-red-500">{jumlah}</p>
+      </div>
+    </div>
+  );
+};
 
 const Covid19 = () => {
   return (
     <React.Fragment>
-      <h1 className="font-bold text-lg mt-5 text-yellow-500">Indonesia Corona Live Data</h1>
-    
-     
+      <h1 className="font-bold text-lg mt-5 text-yellow-500">
+        Indonesia Corona Live Data
+      </h1>
+
       <div className="flex flex-wrap mt-10 gap-x-3 gap-y-3">
-      
-        <div className="flex flex-col w-32 h-32 border-2 border-red-500 py-2 px-2 rounded-md items-center">
-          <p className="font-bold text-sm text-red-500">Meninggal</p>
-          <img src={mati} alt="mati" className="h-10 w-10 my-2"></img>
-          <div>
-            <p className="font-bold text-sm text-red-500">1000000</p>
-          </div>
-        </div>
-       
-        <div className="flex flex-col w-32 h-32 border-2 border-red-500 py-2 px-2 rounded-md items-center">
-          <p className="font-bold text-sm text-red-500">Dirawat</p>
-          <img src={dirawat} alt="dirawat" className="h-10 w-10 my-2"></img>
-          <div>
-            <p className="font-bold text-sm text-red-500">346324140</p>
-          </div>
-        </div>
-
-        <div className="flex flex-col w-32 h-32 border-2 border-red-500 py-2 px-2 rounded-md items-center">
-          <p className="font-bold text-sm text-red-500">Positif</p>
-          <img src={positif} alt="positif" className="h-10 w-10 my-2"></img>
-          <div>
-            <p className="font-bold text-sm text-red-500">34587632</p>
-          </div>
-        </div>
-
-        <div className="flex flex-col w-32 h-32 border-2 border-red-500 py-2 px-2 rounded-md items-center">
-          <p className="font-bold text-sm text-red-500">Sembuh</p>
-          <img src={sembuh} alt="sembuh" className="h-10 w-10 my-2"></img>
-          <div>
-            <p className="font-bold text-sm text-red-500">7658493</p>
-          </div>
-        </div>
-
+        {data.map((x, i) => {
+          return <Card key={i} jumlah={x.jumlah} judul={x.judul} img={x.img} />;
+        })}
       </div>
 
       <p className="text-sm mt-3 text-gray-600">Update terakhir:</p>
-
     </React.Fragment>
+  );
+};
 
-  )
-}
-
-export default Covid19
+export default Covid19;
