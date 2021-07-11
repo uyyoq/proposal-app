f7ef4d72818647434adf0e9dd13817df8f4c0753
indra  susila <indra.susila@logique.co.id>
2021-07-11 17:28:13 +0700

    style(give template): template covid 19
    ]

--- pages/covid-19.js
+++ pages/covid-19.js
@@ -1,16 +1,22 @@
 import Navbar from "../components/Navbar/navbar"
 
+const mati = "/icon/mati.svg"
+
 const Covid19 = () => {
   return (
     <div>
       <div className="flex w-full items-center my-0 mx-auto" >
         <Navbar />
       </div>
-     <div className="flex">
-       <div className=""></div>
-       <div className=""></div>
-       <div className=""></div>
-       <div className=""></div>
+     <div className="flex flex-wrap gap-x-3 gap-y-3">
+       <div className="w-96 border-2 border-red-800 py-2 px-2 h-20">
+         <img src={mati} alt="sedih" className="h-10 w-10"></img>
+         
+         
+      </div>
+       <div className="w-96">andri</div>
+       <div className="w-96">dwi</div>
+       <div className="">nini</div>
       
      </div>
     </div>
