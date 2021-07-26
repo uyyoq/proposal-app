a2facb8cb00733f2975fdcad74417854151f10b5
Kucing <dilkq@gmail.com>
2021-07-26 16:38:15 +0700

    adding text

--- pages/contact.js
+++ pages/contact.js
@@ -6,6 +6,7 @@ const Contact = () => {
   return (
     <React.Fragment>
       <div className="w-11/12 md:w-3/4 mx-auto">
+        <p>Form</p> 
         <FormText />
       </div>
     </React.Fragment>
