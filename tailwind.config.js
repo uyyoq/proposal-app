38db17ad0a43aa0bb03bc99729ea9a4683bf3769
Kucing <dilkq@gmail.com>
2021-07-26 17:17:14 +0700

    adding purge and plugins in thailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,21 @@
 module.exports = {
-  purge: [],
+  plugins: [
+    require("tailwindcss"),
+    require("autoprefixer"),
+  ],
+  purge: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./layout/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     fontFamily: {
       'sans': ['Open Sans', 'Lato', 'Arial', 'sans-serif'
       ],
       'serif': ['Georgia', 'Cambria'],
-      'padauk':['padauk'],
-      'roboto':['roboto'],
-      'poppins':['poppins'],
+      'padauk': ['padauk'],
+      'roboto': ['roboto'],
+      'poppins': ['poppins'],
       'staatliches': ['staatliches'],
       'mono': ['SFMono-Regular', 'Menlo'],
       'display': ['Oswald'],
